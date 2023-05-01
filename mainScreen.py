9bf220c0b9a572149be7a6e0b3c880110a9abcd0
1Tae <oneteak1@gmail.com>
2023-05-01 17:50:48 +0900

    총합, 주문 내역 기능 추가

--- mainScreen.py
+++ mainScreen.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QLabel,
-                             QMainWindow, QGridLayout, QHBoxLayout, QSpinBox)
+                             QMainWindow, QGridLayout, QHBoxLayout, QSpinBox, QMessageBox)
 import sys
 
 
@@ -20,12 +20,14 @@ class CoffeeKiosk(QWidget):
         am_img.setPixmap(am_pixmap)
 
         # 아메리카노 가격 추가
-        am_price = QLabel('4500원', self)
+        self.am_price = 4500
+        am_price_label = QLabel(f'{self.am_price}원', self)
 
         # 아메리카노 개수 선택
-        am_spinbox = QSpinBox(self)
-        am_spinbox.setMinimum(0)
-        am_spinbox.setMaximum(20)
+        self.am_spinbox = QSpinBox(self)
+        self.am_spinbox.setMinimum(0)
+        self.am_spinbox.setMaximum(20)
+        self.am_spinbox.valueChanged.connect(self.update_total_price)
 
         # 아메리카노 버튼 추가
         btn1 = QPushButton('아메리카노', self)
@@ -38,12 +40,14 @@ class CoffeeKiosk(QWidget):
         latte_img.setPixmap(latte_pixmap)
 
         # 카페라떼 가격 추가
-        latte_price = QLabel('5000원', self)
+        self.latte_price = 5000
+        latte_price_label = QLabel(f'{self.latte_price}원', self)
         
         # 카페라떼 개수 선택
-        latte_spinbox = QSpinBox(self)
-        latte_spinbox.setMinimum(0)
-        latte_spinbox.setMaximum(20)
+        self.latte_spinbox = QSpinBox(self)
+        self.latte_spinbox.setMinimum(0)
+        self.latte_spinbox.setMaximum(20)
+        self.latte_spinbox.valueChanged.connect(self.update_total_price)
 
         # 카페라떼 버튼 추가
         btn2 = QPushButton('카페라떼', self)
@@ -56,12 +60,15 @@ class CoffeeKiosk(QWidget):
         capp_img.setPixmap(capp_pixmap)
 
         # 카푸치노 가격 추가
-        capp_price = QLabel('5000원', self)
+        self.capp_price = 5000
+        capp_price_label = QLabel(f'{self.capp_price}원', self)
+        
 
         # 카푸치노 개수 선택
-        capp_spinbox = QSpinBox(self)
-        capp_spinbox.setMinimum(0)
-        capp_spinbox.setMaximum(20)
+        self.capp_spinbox = QSpinBox(self)
+        self.capp_spinbox.setMinimum(0)
+        self.capp_spinbox.setMaximum(20)
+        self.capp_spinbox.valueChanged.connect(self.update_total_price)
 
         # 카푸치노 버튼 추가
         btn3 = QPushButton('카푸치노', self)
@@ -73,24 +80,28 @@ class CoffeeKiosk(QWidget):
         buy_btn.setStyleSheet("background-color: #A0522D; color: white;")
         buy_btn.clicked.connect(self.buy)
 
+        # 총 가격 레이블 추가
+        self.total_price_label = QLabel('총 가격: 0원', self)
+
         # 수직 박스 레이아웃에 위젯 추가
         vbox = QVBoxLayout()
         vbox.setAlignment(Qt.AlignCenter)
         vbox.addWidget(am_img)
-        vbox.addWidget(am_price)
-        vbox.addWidget(am_spinbox)
+        vbox.addWidget(am_price_label)
+        vbox.addWidget(self.am_spinbox)
         vbox.addWidget(btn1)
 
         vbox.addWidget(latte_img)
-        vbox.addWidget(latte_price)
-        vbox.addWidget(latte_spinbox)
+        vbox.addWidget(latte_price_label)
+        vbox.addWidget(self.latte_spinbox)
         vbox.addWidget(btn2)
 
         vbox.addWidget(capp_img)
-        vbox.addWidget(capp_price)
-        vbox.addWidget(capp_spinbox)
+        vbox.addWidget(capp_price_label)
+        vbox.addWidget(self.capp_spinbox)
         vbox.addWidget(btn3)
 
+        vbox.addWidget(self.total_price_label, alignment=Qt.AlignRight)
         vbox.addWidget(buy_btn, alignment=Qt.AlignRight)
 
         self.setLayout(vbox)
@@ -106,8 +117,37 @@ class CoffeeKiosk(QWidget):
     def cappuccino(self):
         pass
 
+    # def buy(self):
+    #     pass
+    def update_total_price(self):
+        am_count = self.am_spinbox.value()
+        latte_count = self.latte_spinbox.value()
+        capp_count = self.capp_spinbox.value()
+
+        total_price = self.am_price * am_count + self.latte_price * latte_count + self.capp_price * capp_count
+        self.total_price_label.setText(f'총 가격: {total_price}원')
+
     def buy(self):
-        pass
+        am_count = self.am_spinbox.value()
+        latte_count = self.latte_spinbox.value()
+        capp_count = self.capp_spinbox.value()
+
+        total_price = self.am_price * am_count + self.latte_price * latte_count + self.capp_price * capp_count
+        self.total_price_label.setText(f'총 가격: {total_price}원')
+
+         # 선택한 커피와 개수 출력
+        order = ''
+        if am_count > 0:
+            order += f'아메리카노 {am_count}잔\n'
+        if latte_count > 0:
+            order += f'카페라떼 {latte_count}잔\n'
+        if capp_count > 0:
+            order += f'카푸치노 {capp_count}잔\n'
+
+        if order:
+            QMessageBox.information(self, '주문 내역', order)
+        else:
+            QMessageBox.warning(self, '주문 실패', '커피를 선택해주세요.')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
