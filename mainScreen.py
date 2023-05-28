5ad4d516ee2307ddb7cfbb18deafeab3a15c9238
1Tae <oneteak1@gmail.com>
2023-05-28 17:03:11 +0900

    관리자 창 생성 완료\\1열 size 문제 존재

--- mainScreen.py
+++ mainScreen.py
@@ -1,3 +1,4 @@
+import sqlite3
 import sys
 import os
 from google.cloud import dialogflow
@@ -79,7 +80,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 아메리카노 이미지 추가
         am_img = QLabel(self)
-        am_pixmap = QPixmap('pictures/americano.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        am_pixmap = QPixmap('pictures/ice_americano.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         am_img.setPixmap(am_pixmap)
 
         # 아메리카노 가격 추가
@@ -99,7 +100,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 카페라떼 이미지 추가
         latte_img = QLabel(self)
-        latte_pixmap = QPixmap('pictures/latte.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        latte_pixmap = QPixmap('pictures/hot_latte.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         latte_img.setPixmap(latte_pixmap)
 
         # 카페라떼 가격 추가
@@ -119,7 +120,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 카푸치노 이미지 추가
         capp_img = QLabel(self)
-        capp_pixmap = QPixmap('pictures/cappuccino.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        capp_pixmap = QPixmap('pictures/hot_cappuccino.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         capp_img.setPixmap(capp_pixmap)
 
         # 카푸치노 가격 추가
@@ -139,7 +140,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 카라멜마끼야또 이미지 추가
         caramel_img = QLabel(self)
-        caramel_pixmap = QPixmap('pictures/caramelMacchiato.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        caramel_pixmap = QPixmap('pictures/hot_caramelMacchiato.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         caramel_img.setPixmap(caramel_pixmap)
 
         # 카라멜마끼야또 가격 추가
@@ -158,7 +159,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 콜드블루 이미지 추가
         cb_img = QLabel(self)
-        cb_pixmap = QPixmap('pictures/coldbrew.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        cb_pixmap = QPixmap('pictures/ice_coldbrew.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         cb_img.setPixmap(cb_pixmap)
 
         # 콜드블루 가격 추가
@@ -178,7 +179,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 돌체콜드블루 이미지 추가
         dcb_img = QLabel(self)
-        dcb_pixmap = QPixmap('pictures/dolceColdbrew.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        dcb_pixmap = QPixmap('pictures/ice_dolceColdbrew.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         dcb_img.setPixmap(dcb_pixmap)
 
         # 돌체콜드블루 가격 추가
@@ -198,7 +199,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 바닐라 아포카토 이미지 추가
         vaff_img = QLabel(self)
-        vaff_pixmap = QPixmap('pictures/vanillaAffogato.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        vaff_pixmap = QPixmap('pictures/ice_vanillaAffogato.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         vaff_img.setPixmap(vaff_pixmap)
 
         # 바닐라 아포카토 가격 추가
@@ -218,7 +219,7 @@ class CoffeeKiosk(QMainWindow):
 
         # 화이트 초콜릿 모카 이미지 추가
         wcm_img = QLabel(self)
-        wcm_pixmap = QPixmap('pictures/whiteChocolateMocha.jpg').scaled(200, 200, Qt.KeepAspectRatio)
+        wcm_pixmap = QPixmap('pictures/ice_whiteChocolateMocha.jpg').scaled(200, 200, Qt.KeepAspectRatio)
         wcm_img.setPixmap(wcm_pixmap)
 
         # 화이트 초콜릿 모카 가격 추가
@@ -563,11 +564,37 @@ class CoffeeKiosk(QMainWindow):
         if dialog.exec_() == QDialog.Accepted:
             id, pw = dialog.get_id_pw()
             if id == 'admin' and pw == '1111':
-                QMessageBox.information(self, '로그인 성공', '로그인에 성공했습니다.')
+                self.show_new_window()
+                #QMessageBox.information(self, '로그인 성공', '로그인에 성공했습니다.')
             else:
                 QMessageBox.warning(self, '로그인 실패', '아이디 또는 비밀번호가 올바르지 않습니다.')
-            
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = CoffeeKiosk()
-    sys.exit(app.exec_())
+
+    def show_new_window(self):
+        self.new_window = QMainWindow()
+        self.new_window.setWindowTitle('관리자 창')
+
+
+        label = QLabel("새로운 창")
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.new_window.setCentralWidget(widget)
+        self.new_window.show()
+
+    def closeEvent(self, event):
+        if self.new_window is not None:
+            self.new_window.close()
+        event.accept()
+
+
+# 메인 애플리케이션 실행
+app = QApplication([])
+window = CoffeeKiosk()
+# window.show_admin_login()
+app.exec_()
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     window = CoffeeKiosk()
+#     sys.exit(app.exec_())
